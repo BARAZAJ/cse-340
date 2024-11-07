73771490102200b82231ea9cbeba57bb4939c79e
BARAZAJ <barazajohn52@gmail.com>
2024-11-07 14:34:57 +0300

    new

--- server.js
+++ server.js
@@ -6,15 +6,32 @@
  * Require Statements
  *************************/
 const express = require("express")
+expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+
+
+
+
+
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
-
+//index route
+app.get("/", function(req,res){
+  res.render("index", {title: "Home"})
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
